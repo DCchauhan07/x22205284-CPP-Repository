128019a4ab908e7cf9de06a1a8fc69f6941d21fb
DCchauhan07 <x22205284@student.ncirl.ie>
2024-04-20 14:34:22 +0000

    Changes for library

--- recipes/views.py
+++ recipes/views.py
@@ -3,32 +3,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Recipe
 from django.urls import reverse_lazy
-import random
-
-def foodquote():
-    quotes = [
-        "Life is uncertain. Eat dessert first.",
-        "Good food is good mood.",
-        "There is no love sincerer than the love of food.",
-        "All you need is love. But a little chocolate now and then doesn't hurt.",
-        "Cooking is love made visible.",
-        "One cannot think well, love well, sleep well, if one has not dined well.",
-        "Food is symbolic of love when words are inadequate.",
-        "People who love to eat are always the best people.",
-        "Laughter is brightest in the place where the food is.",
-        "Food is the ingredient that binds us together.",
-        "The only thing I like better than talking about food is eating.",
-        "The secret ingredient is always love.",
-        "Food tastes better when you eat it with your family.",
-        "Eating is a necessity but cooking is an art.",
-        "Happiness is homemade.",
-        "There is no sincerer love than the love of food.",
-        "First we eat, then we do everything else."
-    ]
-    return random.choice(quotes)
-
-print('hey')
-print(foodquote())
+from foodproj.food_library.food_library.food import foodquote
 
 # Create your views here.
 def home(request):
